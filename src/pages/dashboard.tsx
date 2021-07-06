68b4e1d3a5d34906d873ba9b39e7b12333717a23
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-06 00:06:32 -0300

    Header quase finalizado

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
-//import { Header } from "../components/Header";
+import { Header } from "../components/Header";
 
 
 export default function Dashboard() {
     return(
-//        <Header />
+        <Header />
     )
 }
