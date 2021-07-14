2b3c95ae8113a04d7cb109c4e20fd6b42459ad1e
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-13 22:07:03 -0300

    form de criação de usuário

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,7 +6,38 @@ import { Input } from '../../components/Form/Input'
 import { Header } from '../../components/Header'
 import { Siderbar } from '../../components/Sidebar/index'
 
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+type CreateUserFormData = {
+    nome: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
+  
+  const createUserFormSchema = yup.object().shape({
+    name: yup.string().required('Nome obrigatório'),
+    email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+    password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+    password_confirmation: yup.string().oneOf([
+        null, yup.ref('password')
+    ], 'As senhas precisam ser iguais')
+  })
+
+
 export default function CreateUser() {
+
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver( createUserFormSchema )
+    })
+    const { errors } = formState
+
+    const handleCreateUser: submitHandler = (values) => {
+        
+    }
+
     return(
         <Box>
             <Header />
